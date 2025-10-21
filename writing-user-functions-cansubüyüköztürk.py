1188bc94244811e81c3ddefddf41f33985472e68
cansubuyukozturk <cansu.buyukozturk@std.yeditepe.edu.tr>
2025-10-21 10:54:29 +0300

    Add files via upload

---
+++ writing-user-functions-cansubüyüköztürk.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def throw_rock(m, v0, thetha):
+    g = 9.81 # in m/s^2
+    thetha = thetha * np.pi/180 # in rad.
+    tf = 2*v0*np.sin(thetha)/g # in s
+    hm = v0**2*np.sin(thetha)**2 / (2*g) # in m
+    R = v0**2 * np.sin(2*thetha) /g # in m
+    vh = v0*np.cos(thetha) # in m/s
+    Kh = 1/2*m*vh*hm**2 # in j
+
+    print(''For a rock with %5.3f kg mass thrown with %5.3f m/s ''/
+            at an angle of %6.2f degrees :\n''\
+          ''Time of flight is %10.1e s\n''\
+          ''The range in x-direction is %10.1e m\n''
+          ''Maximum height is %10.1e m\n''\
+          ''The speed at maximum height is %10.1e m/s\n''\
+          ''Kinetic energy at the maximum height is %8.2e )'' % (m, v0, thetha*180/np.pi, tf, R, hm, vh, Kh))
+T   return tf, R, hm, vh, Kh
+myresult = throw_rock(1.5, 0.3, 35.20)
